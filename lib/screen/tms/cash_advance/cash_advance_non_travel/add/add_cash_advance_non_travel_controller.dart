475b78dba9ffc9107696e530796785a9ceee167f
fajarainul14 <fajarainul14@gmail.com>
2023-06-02 08:21:33 +0700

    add currency when add ca non travel

--- lib/screen/tms/cash_advance/cash_advance_non_travel/add/add_cash_advance_non_travel_controller.dart
+++ lib/screen/tms/cash_advance/cash_advance_non_travel/add/add_cash_advance_non_travel_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gais/base/base_controller.dart';
 import 'package:gais/data/model/cash_advance/cash_advance_detail_model.dart';
 import 'package:gais/data/model/cash_advance/cash_advance_model.dart';
+import 'package:gais/data/model/master/currency/currency_model.dart';
 import 'package:gais/data/repository/cash_advance/cash_advance_non_travel_repository.dart';
 import 'package:gais/data/storage_core.dart';
 import 'package:gais/reusable/snackbar/custom_get_snackbar.dart';
@@ -9,15 +10,19 @@ import 'package:gais/screen/tms/cash_advance/cash_advance_non_travel/cash_advanc
 import 'package:gais/screen/tms/cash_advance/cash_advance_non_travel/edit/edit_cash_advance_non_travel_screen.dart';
 import 'package:gais/util/ext/int_ext.dart';
 import 'package:gais/util/ext/string_ext.dart';
+import 'package:gais/util/mixin/master_data_mixin.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
-class AddCashAdvanceNonTravelController extends BaseController {
+class AddCashAdvanceNonTravelController extends BaseController with MasterDataMixin{
   final TextEditingController dateController = TextEditingController();
   final TextEditingController eventController = TextEditingController();
   final TextEditingController totalController = TextEditingController();
   final formKey = GlobalKey<FormState>();
   DateFormat dateFormat = DateFormat("dd/MM/yyyy");
+  final listCurrency = <CurrencyModel>[].obs;
+
+  final selectedCurrency = CurrencyModel().obs;
 
   List<CashAdvanceDetailModel> listDetail = <CashAdvanceDetailModel>[];
 
@@ -31,6 +36,19 @@ class AddCashAdvanceNonTravelController extends BaseController {
     totalController.text = _getTotal();
     super.onInit();
   }
+
+  @override
+  void onReady() {
+    super.onReady();
+    initData();
+  }
+
+  void initData()async{
+    final currencies = await getListCurrency();
+    listCurrency(currencies);
+    selectedCurrency(listCurrency.first);
+  }
+
 
   void addItem(CashAdvanceDetailModel item) {
     listDetail.add(item);
@@ -73,6 +91,7 @@ class AddCashAdvanceNonTravelController extends BaseController {
       idEmployee: userId.toInt(),
       typeCa: "2",
       event: eventController.text,
+      idCurrency: selectedCurrency.value.id,
       date: dateController.text
           .toDateFormat(targetFormat: "yyyy-MM-dd", originFormat: "dd/MM/yyyy"),
       arrayDetail: listDetail,
@@ -90,4 +109,10 @@ class AddCashAdvanceNonTravelController extends BaseController {
           arguments: {"item": cashAdvanceModel});
     });
   }
+
+  void onChangeSelectedCurrency(String id) {
+    final selected = listCurrency.firstWhere((item) => item.id == id.toInt());
+    selectedCurrency(selected);
+  }
+
 }
