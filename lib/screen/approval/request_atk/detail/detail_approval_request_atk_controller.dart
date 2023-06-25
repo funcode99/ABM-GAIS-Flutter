ff137429d91cf6c8167c2aee51b1bc6744a363a6
fajarainul14 <fajarainul14@gmail.com>
2023-06-26 04:46:06 +0700

    add notification after approval (when error occurred)

--- lib/screen/approval/request_atk/detail/detail_approval_request_atk_controller.dart
+++ lib/screen/approval/request_atk/detail/detail_approval_request_atk_controller.dart
@@ -48,8 +48,9 @@ class DetailApprovalRequestATKController extends BaseController {
   }
 
   void setValue() {
-    createdDateController.text = detailSelectedItem.value.createdAt?.toDateFormat(
-        originFormat: "yyyy-MM-dd", targetFormat: "dd/MM/yyyy") ??
+    createdDateController.text = detailSelectedItem.value.createdAt
+            ?.toDateFormat(
+                originFormat: "yyyy-MM-dd", targetFormat: "dd/MM/yyyy") ??
         "-";
     createdByController.text = detailSelectedItem.value.employeeName ?? "-";
     rejectNoteController.text = detailSelectedItem.value.remarks ?? "-";
@@ -75,30 +76,41 @@ class DetailApprovalRequestATKController extends BaseController {
     });
   }
 
-  void reject()async{
-    final result = await _repository.reject(approvalModel.value, selectedItem.value.id!);
-    result.fold((l) => null, (r) {
-      if(r){
-        showApprovalSuccessDialog("The request was successfully rejected!".tr).then((value) => Get.back(result: true));
-      }else{
-        showApprovalFailDialog("Request failed to be rejected!".tr).then((value) => Get.back(result: true));
+  void reject() async {
+    final result =
+        await _repository.reject(approvalModel.value, selectedItem.value.id!);
+    result.fold(
+        (l) => showApprovalFailDialog("Request failed to be approved!".tr)
+            .then((value) => Get.back(result: true)), (r) {
+      if (r) {
+        showApprovalSuccessDialog("The request was successfully rejected!".tr)
+            .then((value) => Get.back(result: true));
+      } else {
+        showApprovalFailDialog("Request failed to be rejected!".tr)
+            .then((value) => Get.back(result: true));
       }
     });
   }
 
-  void approve()async{
-    final result = await _repository.approve(approvalModel.value, selectedItem.value.id!);
-    result.fold((l) => null, (r) {
-      if(r){
-        showApprovalSuccessDialog("The request was successfully approved!".tr).then((value) => Get.back(result: true));
-      }else{
-        showApprovalFailDialog("Request failed to be approved!".tr).then((value) => Get.back(result: true));
+  void approve() async {
+    final result =
+        await _repository.approve(approvalModel.value, selectedItem.value.id!);
+    result.fold(
+        (l) => showApprovalFailDialog("Request failed to be approved!".tr)
+            .then((value) => Get.back(result: true)), (r) {
+      if (r) {
+        showApprovalSuccessDialog("The request was successfully approved!".tr)
+            .then((value) => Get.back(result: true));
+      } else {
+        showApprovalFailDialog("Request failed to be approved!".tr)
+            .then((value) => Get.back(result: true));
       }
     });
   }
 
-  void getApprovalLog()async{
-    final result = await _repository.getApprovalLog(detailSelectedItem.value.id!);
+  void getApprovalLog() async {
+    final result =
+        await _repository.getApprovalLog(detailSelectedItem.value.id!);
 
     result.fold((l) => null, (r) {
       listLogApproval.value = r;
