a07cd2d9758a119173ea7a7c4c2d789c0741c396
fajarainul14 <fajarainul14@gmail.com>
2023-07-13 19:19:50 +0700

    fix filter, handle same date approval cash advance non travel

--- lib/screen/approval/cash_advance_non_travel/list/approval_cash_advance_non_travel_list_screen.dart
+++ lib/screen/approval/cash_advance_non_travel/list/approval_cash_advance_non_travel_list_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:gais/const/color.dart';
 import 'package:gais/const/textstyle.dart';
 import 'package:gais/reusable/bottombar.dart';
+import 'package:gais/reusable/calendar/custom_calendar_picker.dart';
 import 'package:gais/reusable/custombackbutton.dart';
 import 'package:gais/reusable/customiconbutton.dart';
 import 'package:gais/reusable/customsearchbar.dart';
@@ -76,7 +77,10 @@ class _ApprovalCashAdvanceNonTravelListScreenState
                         controller: controller.dateRangeController,
                         suffixIcon: const Icon(Icons.calendar_month),
                         onTap: () {
-                          showCustomDateRangePicker(
+                          if(DateUtils.isSameDay(controller.startDateTemp.value, controller.endDateTemp.value)){
+                            controller.endDateTemp.value = null;
+                          }
+                          showCustomCalendarPicker(
                             context,
                             dismissible: true,
                             minimumDate: DateTime.now()
@@ -87,13 +91,18 @@ class _ApprovalCashAdvanceNonTravelListScreenState
                             startDate: controller.startDateTemp.value,
                             backgroundColor: Colors.white,
                             primaryColor: Colors.green,
+                            isRange: false,
                             onApplyClick: (start, end) {
-                              controller.endDateTemp.value = end;
+                              controller.endDateTemp.value = end ?? start;
                               controller.startDateTemp.value = start;
                               controller.dateRangeController.text =
-                                  "${controller.dateFormat.format(start)} - ${controller.dateFormat.format(end)}";
+                                  "${controller.dateFormat.format(controller.startDateTemp.value!)} - ${controller.dateFormat.format(controller.endDateTemp.value!)}";
                             },
-                            onCancelClick: () {},
+                            onCancelClick: () {
+                              if(controller.startDateTemp.value != null){
+                                controller.endDateTemp.value ??= controller.startDateTemp.value;
+                              }
+                            },
                           );
                         },
                         label: "Date Range".tr),
