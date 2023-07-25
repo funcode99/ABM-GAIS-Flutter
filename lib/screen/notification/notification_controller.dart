c9cbf35cdd4227c5f332efb729b367afcab067cf
fajarainul14 <fajarainul14@gmail.com>
2023-07-26 05:18:27 +0700

    show approval notification

--- lib/screen/notification/notification_controller.dart
+++ lib/screen/notification/notification_controller.dart
@@ -7,9 +7,15 @@ import 'package:gais/data/storage_core.dart';
 import 'package:gais/reusable/snackbar/custom_get_snackbar.dart';
 import 'package:get/get.dart';
 
-class NotificationController extends BaseController{
+enum NotificationTabEnum{
+  notification,
+  approval
+}
+class NotificationController extends BaseController with GetTickerProviderStateMixin{
+  late TabController tabController;
 
   final listNotification = <NotificationModel>[].obs;
+  final listNotificationApproval = <NotificationModel>[].obs;
 
   final NotificationRepository _repository = Get.find();
   late PaginationModel? paginationModel;
@@ -17,12 +23,25 @@ class NotificationController extends BaseController{
   final currentPage = 1.obs;
   int limit = 10;
 
+  late PaginationModel? paginationModelApproval;
+  final totalPageApproval = 1.obs;
+  final currentPageApproval = 1.obs;
+
   final isApproval = false.obs;
+  final tabs = <NotificationTabEnum>[].obs;
 
   @override
   void onInit() {
     super.onInit();
+    tabController = TabController(length: tabs.length, vsync: this);
     getNotification();
+    getNotificationApproval();
+  }
+
+  @override
+  void onClose() {
+    tabController.dispose();
+    super.onClose();
   }
 
   @override
@@ -32,8 +51,15 @@ class NotificationController extends BaseController{
   }
 
   void initData()async{
-    String roleId = await storage.readString(StorageCore.siteID);
+    String roleId = await storage.readString(StorageCore.roleID);
+
     isApproval.value = roleId == "1" || roleId == "2" || roleId == "3" ? true : false;
+
+    tabs.add(NotificationTabEnum.notification);
+    if(isApproval.value){
+      tabs.add(NotificationTabEnum.approval);
+    }
+    tabController = TabController(length: tabs.length, vsync: this);
 
   }
 
@@ -47,8 +73,8 @@ class NotificationController extends BaseController{
 
     result.fold(
             (l) {
-          Get.showSnackbar(
-              CustomGetSnackBar(message: l.message, backgroundColor: Colors.red));
+          /*Get.showSnackbar(
+              CustomGetSnackBar(message: l.message, backgroundColor: Colors.red));*/
           listNotification.clear();
           totalPage(1);
           currentPage(1);
@@ -64,6 +90,57 @@ class NotificationController extends BaseController{
               .toList();
           listNotification.refresh();
         });
+  }
+
+  void getNotificationApproval({int page = 1}) async {
+    final result = await _repository.getNotificationApproval(
+        data: {
+          "page" : page,
+          "perPage" : limit,
+        }
+    );
+
+    result.fold(
+            (l) {
+          /*Get.showSnackbar(
+              CustomGetSnackBar(message: l.message, backgroundColor: Colors.red));
+          listNotification.clear();*/
+          totalPageApproval(1);
+          currentPageApproval(1);
+        },
+            (r) {
+          paginationModelApproval = r;
+          int tempTotalPage = (paginationModelApproval!.total!/limit).ceil();
+          totalPageApproval(tempTotalPage);
+          currentPageApproval(paginationModelApproval?.currentPage);
+
+          listNotificationApproval.value = paginationModelApproval!.data!
+              .map((e) => NotificationModel.fromJson(e))
+              .toList();
+          listNotificationApproval.refresh();
+        });
+  }
+
+  int unreadMessageCount(){
+    int result = 0;
+    listNotification.forEach((element) {
+      if(element.isViewed!=1){
+        result++;
+      }
+    });
+
+    return result;
+  }
+
+  int unreadMessageCountApproval(){
+    int result = 0;
+    listNotificationApproval.forEach((element) {
+      if(element.isViewed!=1){
+        result++;
+      }
+    });
+
+    return result;
   }
 
 }
--- lib/screen/notification/notification_screen.dart
+++ lib/screen/notification/notification_screen.dart
@@ -8,22 +8,8 @@ import 'package:gais/screen/notification/notification_controller.dart';
 import 'package:get/get.dart';
 import 'package:iconly/iconly.dart';
 
-class NotificationScreen extends StatefulWidget {
+class NotificationScreen extends StatelessWidget {
   const NotificationScreen({Key? key}) : super(key: key);
-
-  @override
-  State<NotificationScreen> createState() => _NotificationScreenState();
-}
-
-class _NotificationScreenState extends State<NotificationScreen>
-    with TickerProviderStateMixin {
-  late TabController tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(length: 2, vsync: this);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,101 +24,143 @@ class _NotificationScreenState extends State<NotificationScreen>
               borderRadius: BorderRadius.circular(10)
           ),
           child: Obx(() {
-            List<Widget> tabBars = [
-              Tab(
-                icon: Container(
-                  alignment: Alignment.center,
-                  height: 30,
-                  width: 30,
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      color: baseColor,
-                      borderRadius: BorderRadius.circular(50)
-                  ),
-                  child: Text("${controller.listNotification.length}",
-                    style: listSubTitleTextStyle.copyWith(color: infoColor),),
-                ),
-                text: "Notification",
-              )
-            ];
-
-            if(controller.isApproval.value){
-              tabBars.add(Tab(
-                icon: Container(
-                  alignment: Alignment.center,
-                  height: 30,
-                  width: 30,
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      color: baseColor,
-                      borderRadius: BorderRadius.circular(50)
-                  ),
-                  child: Text("${controller.listNotification.length + 1}",
-                    style: listSubTitleTextStyle.copyWith(color: infoColor),),
-                ),
-                text: "Approval",
-              ));
-            }
-            
-            tabController = TabController(length: tabBars.length, vsync: this);
             return TabBar(
-              controller: tabController,
+              controller: controller.tabController,
               labelColor: infoColor,
               unselectedLabelColor: greyColor,
               indicatorSize: TabBarIndicatorSize.label,
-              tabs: tabBars,
+              tabs: controller.tabs.mapIndexed(
+                      (index, element){
+                        return Tab(
+                          icon: Container(
+                            alignment: Alignment.center,
+                            height: 30,
+                            width: 30,
+                            padding: const EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: baseColor,
+                                borderRadius: BorderRadius.circular(50)
+                            ),
+                            child: Text(
+                              "${element == NotificationTabEnum.notification ? controller.unreadMessageCount() : controller.unreadMessageCountApproval()}",
+                              style: listSubTitleTextStyle.copyWith(color: infoColor),),
+                          ),
+                          text: element == NotificationTabEnum.notification ? "Notification" : "Approval",
+                        );
+                      }).toList(),
             );
           }),
         ),
         Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                children: [
-                  Obx(() {
-                    return CustomPagination(
-                      colorSub: whiteColor,
-                      colorPrimary: infoColor,
-                      key: UniqueKey(),
-                      onPageChanged: (page) {
-                        if (page != controller.currentPage.value) {
-                          controller.getNotification(page: page);
-                        }
-                      },
-                      pageTotal: controller.totalPage.value,
-                      margin: EdgeInsets.zero,
-                      pageInit: controller.currentPage.value,
-                    );
-                  }),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Expanded(
-                      child: RefreshIndicator(
-                        onRefresh: () async {
-                          controller.getNotification();
-                        },
-                        child: Obx(() {
-                          return controller.listNotification.isEmpty
-                              ? const DataEmpty()
-                              : ListView(
-                            children: [
-                              ...controller.listNotification.mapIndexed((index,
-                                  item) =>
-                                  NotificationItem(
-                                    text: item.text ?? "",
-                                    date: item.date ?? "",
-                                    name: item.name ?? "",
-                                    showIndicator: item.isViewed != 1,
-                                  )
-                              )
-                            ],
+            child: Obx(() {
+              return TabBarView(
+                controller: controller.tabController,
+                children: controller.tabs.mapIndexed(
+                      (index, element){
+                        if(element == NotificationTabEnum.notification){
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Column(
+                              children: [
+                                Obx(() {
+                                  return CustomPagination(
+                                    colorSub: whiteColor,
+                                    colorPrimary: infoColor,
+                                    key: UniqueKey(),
+                                    onPageChanged: (page) {
+                                      if (page != controller.currentPage.value) {
+                                        controller.getNotification(page: page);
+                                      }
+                                    },
+                                    pageTotal: controller.totalPage.value,
+                                    margin: EdgeInsets.zero,
+                                    pageInit: controller.currentPage.value,
+                                  );
+                                }),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                Expanded(
+                                    child: RefreshIndicator(
+                                      onRefresh: () async {
+                                        controller.getNotification();
+                                      },
+                                      child: Obx(() {
+                                        return controller.listNotification.isEmpty
+                                            ? const DataEmpty()
+                                            : ListView(
+                                          children: [
+                                            ...controller.listNotification.mapIndexed((
+                                                index,
+                                                item) =>
+                                                NotificationItem(
+                                                  text: item.text ?? "",
+                                                  date: item.date ?? "",
+                                                  name: item.name ?? "",
+                                                  showIndicator: item.isViewed != 1,
+                                                )
+                                            )
+                                          ],
+                                        );
+                                      }),
+                                    )),
+                              ],
+                            ),
                           );
-                        }),
-                      )),
-                ],
-              ),
-            )
+                        }
+
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            children: [
+                              Obx(() {
+                                return CustomPagination(
+                                  colorSub: whiteColor,
+                                  colorPrimary: infoColor,
+                                  key: UniqueKey(),
+                                  onPageChanged: (page) {
+                                    if (page != controller.currentPageApproval.value) {
+                                      controller.getNotificationApproval(page: page);
+                                    }
+                                  },
+                                  pageTotal: controller.totalPageApproval.value,
+                                  margin: EdgeInsets.zero,
+                                  pageInit: controller.currentPageApproval.value,
+                                );
+                              }),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Expanded(
+                                  child: RefreshIndicator(
+                                    onRefresh: () async {
+                                      controller.getNotificationApproval();
+                                    },
+                                    child: Obx(() {
+                                      return controller.listNotificationApproval.isEmpty
+                                          ? const DataEmpty()
+                                          : ListView(
+                                        children: [
+                                          ...controller.listNotificationApproval.mapIndexed((
+                                              index,
+                                              item) =>
+                                              NotificationItem(
+                                                text: item.text ?? "",
+                                                date: item.date ?? "",
+                                                name: item.name ?? "",
+                                                showIndicator: item.isViewed != 1,
+                                              )
+                                          )
+                                        ],
+                                      );
+                                    }),
+                                  )),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+              );
+            })
         )
       ],
     );
@@ -147,7 +175,7 @@ class NotificationItem extends StatelessWidget {
   final String date;
   final String text;
   final bool showIndicator;
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -181,7 +209,8 @@ class NotificationItem extends StatelessWidget {
                       color: redColor
                   ),
                 ),
-              ) else const SizedBox(),
+              ) else
+                const SizedBox(),
             ],
           ),
           const SizedBox(
