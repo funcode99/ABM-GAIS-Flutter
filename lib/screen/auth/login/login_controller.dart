ae34a82eb53095142539d506f5e44bcc7e1b1a19
fajarainul14 <fajarainul14@gmail.com>
2023-08-07 03:40:01 +0700

    change snackbar position for login

--- lib/screen/auth/login/login_controller.dart
+++ lib/screen/auth/login/login_controller.dart
@@ -91,6 +91,10 @@ class LoginController extends BaseController {
                 isDismissible: true,
                 duration: Duration(seconds: 3),
                 backgroundColor: Colors.green,
+                snackPosition: SnackPosition.TOP,
+                snackStyle: SnackStyle.FLOATING,
+                margin: EdgeInsets.only(top: 50),
+                animationDuration: Duration(milliseconds: 500),
               ),
             ),
           )
