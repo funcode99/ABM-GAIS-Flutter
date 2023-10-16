224bbf9da1b39ad03b68e14dfff36f786755451d
fajarainul14 <fajarainul14@gmail.com>
2023-10-16 19:11:49 +0700

    integrate api approval log

--- lib/data/repository/actualization_trip/new_actualization_trip_repository.dart
+++ lib/data/repository/actualization_trip/new_actualization_trip_repository.dart
@@ -142,7 +142,22 @@ class NewActualizationTripRepository implements BaseRepository<ActualizationTrip
 
   @override
   Future<Either<BaseError, List<ApprovalLogModel>>> getApprovalLog(dynamic id) async{
-    throw UnimplementedError();
+    try {
+      Dio.Response response = await network.dio.get(
+        '/api/actual_trip/get_history/$id',
+      );
+      ApiResponseModel apiResponseModel = ApiResponseModel.fromJson(response.data, ApprovalLogModel.fromJsonModelList);
+      return right(apiResponseModel.data);
+    } on DioError catch (e) {
+      print("DioError $e");
+      return left(BaseError(message: e.response!.data['message'] ?? e.message));
+    } on FormatException catch (e){
+      print("FormatException $e");
+      return left(BaseError(message: e.message));
+    }catch (e){
+      print("catch error $e");
+      return left(BaseError(message: "General error occurred"));
+    }
   }
 
   @override
